ba7d3da8851d64964600ceff16f54e49e24cbad7
Jeff Chasin <McCloudFace1@gmail.com>
2018-11-14 21:31:05 -0500

    [wip] api endpoints   - add getAtrail, getTags functions in db

--- database/index.js
+++ database/index.js
@@ -1,25 +1,70 @@
 require('dotenv').config();
 
-
-var getAllTrails = (callback) => {
-  const knex = require('knex')({
+var getAtrail = (id, callback) => {
+  var knex = require('knex')({
     client: 'mysql',
     connection: {
-      host: process.env.DB_HEROKU_HOST,
-      user: process.env.DB_HEROKU_USER,
-      password: process.env.DB_HEROKU_PASS,
-      database: 'heroku_7288f22ed8d6924'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'trailService'
     }
   });
   knex.select()
     .from('trail')
-    .then( (rows) => {
-      callback(rows);
+    .where('trail_id', '=', id)
+    .then((row) => {
+      callback(row);
     })
-    .catch( (error) => {
-      console.error(error);
+    .catch((error) => {
+      console.error('getAtrail select error: ', error);
     });
 };
 
+var getTags = (id, callback) => {
+  var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'trailService'
+    }
+  });
+  // TODO
+  // SELECT tag_name FROM tags WHERE tag_id IN (SELECT tag_id FROM trail_tags WHERE trail_id = 1)
+  knex.select('tag_name').from('tags')
+    .wherein('tag_id', function(id) {
+      this.select('tag_id').from('trail_tags').where('trail_id', '=', id);
+    })
+    .then( (tags) => {
+      callback(tags);
+    })
+    .catch( (error) => {
+      console.error('getTags select error: ', error);
+    });
+};
+
+var getAllTrails = (callback) => {
+  var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'trailService'
+    }
+  });
+  knex.select()
+    .from('trail')
+    .then((rows) => {
+      callback(rows);
+    })
+    .catch((error) => {
+      console.error('getAllTrails select error: ', error);
+    });
+};
 
 module.exports.getAllTrails = getAllTrails;
+module.exports.getAtrail = getAtrail;
+module.exports.getTags = getTags;
--- server/index.js
+++ server/index.js
@@ -2,16 +2,29 @@ const express = require('express');
 const db = require('../database/index');
 
 const app = express();
-const port = process.env.PORT;
-if (port === null || port === '') {
-  port = 8000;
+var port = process.env.PORT;
+
+if (!port || port === '') {
+  port = 3000;
 }
 
 // app.get('/', (req, res) => res.send('Howdoo'));
 
+// test out sending all rows from trail table
 app.get('/', (req, res) => {
   db.getAllTrails( (rowsRes) => {
     res.json(rowsRes);
+  });
+});
+
+app.get('/:trailId/trailInfo', (req, res) => {
+  var theId = req.params.trailId;
+  db.getAtrail(theId, (trail) => {
+    // TODO:
+    // getAtrail functioning as-expected
+    // need to call getTags, create array of tags, append to
+    // trail object for response
+    console.log(trail);
   });
 });
 
