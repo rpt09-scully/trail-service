60022a7e4a330bfd3654563eafe4d33c86795315
Jeff Chasin <McCloudFace1@gmail.com>
2018-12-10 22:38:45 -0500

    feat: [wip] static state data

--- client/index.js
+++ client/index.js
@@ -1,19 +1,7 @@
 import Banner from './components/banner.jsx';
 import TrailDescription from './components/traildescription.jsx';
 
-if (window.NT === undefined) {
-  window.NT = {
-    TrailService: {
-      Banner: {},
-      TrailDescription: {}
-    }
-  };
-} else if (window.NT) {
-  window.NT.TrailService = {
-    Banner: {},
-    TrailDescription: {}
-  };
-}
-
+window.NT = window.NT || {}; 
+window.NT.TrailService = window.NT.TrailService || {};
 window.NT.TrailService.Banner = Banner;
 window.NT.TrailService.TrailDescription = TrailDescription;
