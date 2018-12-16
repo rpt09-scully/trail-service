25ade6e8af51a8df8c57f84b5c0c108554fffeae
Jeff Chasin <McCloudFace1@gmail.com>
2018-12-16 11:04:58 -0500

    [wip] style traildescription

--- client/components/traildescription.jsx
+++ client/components/traildescription.jsx
@@ -15,22 +15,29 @@ export default class TrailDescription extends React.Component {
         <div className="col">
           <p>Golden Gate Park Trail is a 6.1 mile heavily trafficked loop trail located near San Francisco, California that features a lake and is good for all skill levels. The trail offers a number of activity options and is accessible year-round. Dogs are also able to use this trail but must be kept on leash.</p>
 
-          <div className="row stats">
+          <div className={`row ${style.stats}`}>
             <div className="col">
-              Distance<br />
-              6.1 miles
+              <div className="d-flex justify-content-center">
+                Distance<br />
+                6.1 miles
+              </div>
             </div>
             <div className="col">
-              Elevation Gain<br />
-              351 feet
+              <div className="d-flex justify-content-center">
+                Elevation Gain<br />
+                351 feet
+              </div>
             </div>
             <div className="col">
-              Route Type<br />
-              Loop
+              <div className="d-flex justify-content-center">
+                Route Type<br />
+                Loop
+              </div>
             </div>
           </div>
+
           <div className="row">
-            <div className="col">
+            <div className={`${style.col} ${style.tags}`}>
               <span>dogs on leash</span> <span>kid friendly</span> <span>birding</span> <span>hiking</span> <span>mountain biking</span>
               <span>nature trips</span> <span>road biking</span> <span>trail running</span> <span>walking</span> <span>forest</span> <span>lake</span>
               <span>partially paved</span>
