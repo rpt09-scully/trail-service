529854c4b0faf3046bddf22ff5fd58b6b1a44151
Jeff Chasin <McCloudFace1@gmail.com>
2018-12-17 21:46:06 -0500

    feat: components render async from new endpoints
      - Add axios requests for component-specific endpoints
      - Refactor to React "Update on Async Rendering" format
      - https://jffc.us/react-async-external-data

--- client/components/banner.jsx
+++ client/components/banner.jsx
@@ -1,58 +1,63 @@
 import React from 'react';
+import axios from 'axios';
 import style from '../trail-style.css';
 
 export default class Banner extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      fetching: true,
-      trail: {}
+      trail: null
     };
   }
 
   componentDidMount() {
-    fetch('http://localhost:3001/1/banner')
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        this.setState( (prevState) => ({
-          trail: json,
-          fetching: false
-        }));
-        // console.log('json :', json.data);
+    this._asyncRequest = axios.get('http://localhost:3001/1/banner')
+      .then(response => {
+        this._asyncRequest = null;
+        this.setState({ trail: response.data });
       })
       .catch((error) => {
-        console.error('fetch banner error: ', error);
+        console.error('axios banner error: ', error);
       });
   }
 
-  render() {
-    return (this.state.fetching) ? (<div className="fetching"></div>) : (
-      <div>
-        {/* Template strings (template literals) used for css module format of Bootstrap class names */}
-        {/* See webpack.config.js css-loader options for localIdentName syntax of css output */}
+  componentWillUnmount() {
+    if (this._asyncRequest) {
+      this._asyncRequest.cancel();
+    }
+  }
 
-        <div id="banner_content" className="row flex-row justify-content-left align-items-end col-12">
-          <div className="jumbotron-fluid d-flex">
-            <img className="heroPhoto img-fluid" src={this.state.trail.photoUrl} alt="hero img" />
-            <div className={style.heroStats}>
-              <h2>{this.state.trail.trailName}</h2>
-              <div className="difficulty">{this.state.trail.difficulty} <span className="reviews">⭐️⭐️⭐️ {this.state.trail.totalReviews} Reviews</span></div>
-              <div className="rank">{this.trail.state.trailRank}</div>
-              <div className="photoCount">{this.state.trail.photosCount} photos</div>
+  render() {
+    if (this.state.trail === null) {
+      // Render loading state...
+      return <div className="fetching"></div>;
+    } else {
+      return (
+        <div>
+          {/* Template strings (template literals) used for css module format of Bootstrap class names */}
+          {/* See webpack.config.js css-loader options for localIdentName syntax of css output */}
+  
+          <div id="banner_content" className="row flex-row justify-content-left align-items-end col-12">
+            <div className="jumbotron-fluid d-flex">
+              <img className="heroPhoto img-fluid" src={this.state.trail.heroUrl} alt="hero img" />
+              <div className={style.heroStats}>
+                <h2>{this.state.trail.trailName}</h2>
+                <div className="difficulty">{this.state.trail.difficulty} <span className="reviews">⭐️⭐️⭐️ {this.state.trail.totalReviews} Reviews</span></div>
+                <div className="rank">{this.state.trail.trailRank}</div>
+                <div className="photoCount">{this.state.trail.photosCount} photos</div>
+              </div>
             </div>
           </div>
+          {/* <!-- little bubble badges--> */}
+          {/* <div id="bubble_menu" className="row flex-row justify-content-around">
+            <div className={style.bubble}></div>
+            <div className={style.bubble}></div>
+            <div className={`style.bubble ${style['d-none']} ${style['d-lg-flex']}`}></div>
+            <div className={`style.bubble ${style['d-none']} ${style['d-lg-flex']}`}></div>
+            <div className={style.bubble}></div>
+          </div> */}
         </div>
-        {/* <!-- little bubble badges--> */}
-        {/* <div id="bubble_menu" className="row flex-row justify-content-around">
-          <div className={style.bubble}></div>
-          <div className={style.bubble}></div>
-          <div className={`style.bubble ${style['d-none']} ${style['d-lg-flex']}`}></div>
-          <div className={`style.bubble ${style['d-none']} ${style['d-lg-flex']}`}></div>
-          <div className={style.bubble}></div>
-        </div> */}
-      </div>
-    );
+      );
+    }
   }
 }
--- client/components/traildescription.jsx
+++ client/components/traildescription.jsx
@@ -1,70 +1,74 @@
 import React from 'react';
+import axios from 'axios';
 import style from '../trail-style.css';
 
 export default class TrailDescription extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      fetching: true,
-      trailDesc: {}
+      trailDesc: null
     };
   }
 
   componentDidMount() {
-    fetch('http://localhost:3001/1/trailDescription')
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        this.setState( (prevState) => ({
-          trailDesc: json,
-          fetching: false
-        }));
-        // console.log('json :', json.data);
+    this._asyncRequest = axios.get('http://localhost:3001/1/trailDescription')
+      .then(response => {
+        this._asyncRequest = null;
+        this.setState({ trailDesc: response.data });
       })
       .catch((error) => {
-        console.error('fetch trailDesc error: ', error);
+        console.error('axios trailDesc error: ', error);
       });
   }
 
+  componentWillUnmount() {
+    if (this._asyncRequest) {
+      this._asyncRequest.cancel();
+    }
+  }
+
   render() {
-    const trailTags = this.state.trailDesc.tags.map((tag) => <span>tag</span>);
+    if (this.state.trailDesc === null) {
+      // Render loading state...
+      return <div className="fetching"></div>;
+    } else {
+      const trailTags = this.state.trailDesc.tags.map((tag, i) => <span key={i}>{tag}</span>);
+      return (
+        <div className="row flex-row justify-content-left col-8">
+          {/* Template strings (template literals) used for css module format of Bootstrap class names */}
+          {/* See webpack.config.js css-loader options for localIdentName syntax of css output */}
+          <div className="col">
+            <p>{this.state.trailDesc.description}</p>
 
-    return (this.state.fetching) ? (<div className="fetching"></div>) : (
-      <div className="row flex-row justify-content-left col-8">
-        {/* Template strings (template literals) used for css module format of Bootstrap class names */}
-        {/* See webpack.config.js css-loader options for localIdentName syntax of css output */}
-        <div className="col">
-          <p>{this.state.trailDesc.description}</p>
-
-          <div className={`row ${style.stats}`}>
-            <div className="col">
-              <div className="d-flex justify-content-center">
-                Distance<br />
-                {this.state.trailDesc.distance} {this.state.trailDesc.distanceUnits}
+            <div className={`row ${style.stats}`}>
+              <div className="col">
+                <div className="d-flex justify-content-center">
+                  Distance<br />
+                  {this.state.trailDesc.distance} {this.state.trailDesc.distanceUnits}
+                </div>
+              </div>
+              <div className="col">
+                <div className="d-flex justify-content-center">
+                  Elevation Gain<br />
+                  {this.state.trailDesc.elevationGain} {this.state.trailDesc.elevationUnits}
+                </div>
+              </div>
+              <div className="col">
+                <div className="d-flex justify-content-center">
+                  Route Type<br />
+                  {this.state.trailDesc.routeType}
+                </div>
               </div>
             </div>
-            <div className="col">
-              <div className="d-flex justify-content-center">
-                Elevation Gain<br />
-                {this.state.trailDesc.elevation} {this.state.trailDesc.elevationUnits}
-              </div>
-            </div>
-            <div className="col">
-              <div className="d-flex justify-content-center">
-                Route Type<br />
-                {this.state.trailDesc.routeType}
-              </div>
-            </div>
-          </div>
 
-          <div className="row">
-            <div className={`${style.col} ${style.tags}`}>
-              {trailTags}
+            <div className="row">
+              <div className={`${style.col} ${style.tags}`}>
+                {trailTags}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
