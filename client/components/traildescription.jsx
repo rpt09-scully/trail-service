ba81a1c3724a64634fa1ec124cea9efa466a3fae
JC <38263920+McCloudFace@users.noreply.github.com>
2018-12-18 18:19:15 -0500

    [review & merge w squash commit] feat: respond to trailId in URL (#32)
    * public.app
    
    * feat: add state endpoints to both components
    
      - each component making separate api calls for pre-shaped responses
    
    * feat: components render async from new endpoints
    
      - Add axios requests for component-specific endpoints
      - Refactor to React "Update on Async Rendering" format
      - https://jffc.us/react-async-external-data
    
    * feat: respond to trailId in URL

--- client/components/traildescription.jsx
+++ client/components/traildescription.jsx
@@ -1,50 +1,76 @@
 import React from 'react';
-import style from '../trail-style.css';
+import axios from 'axios';
+import getTrailId from '../helpers/getId';
+import style from '../css/trailDescStyle.css';
 
 export default class TrailDescription extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      activeTrail: getTrailId(),
+      trailDesc: null
+    };
+  }
+
+  componentDidMount() {
+    this._asyncRequest = axios.get(`http://localhost:3001/${this.state.activeTrail}/trailDescription`)
+      .then(response => {
+        this._asyncRequest = null;
+        this.setState({ trailDesc: response.data });
+      })
+      .catch((error) => {
+        console.error('axios trailDesc error: ', error);
+      });
+  }
+
+  componentWillUnmount() {
+    if (this._asyncRequest) {
+      this._asyncRequest.cancel();
+    }
   }
 
   render() {
-    return (
-      <div className="row flex-row justify-content-left col-8">
-        {/* Template strings (template literals) used for css module format of Bootstrap class names */}
-        {/* See webpack.config.js css-loader options for localIdentName syntax of css output */}
-        <div className="col">
-          <p>Golden Gate Park Trail is a 6.1 mile heavily trafficked loop trail located near San Francisco, California that features a lake and is good for all skill levels. The trail offers a number of activity options and is accessible year-round. Dogs are also able to use this trail but must be kept on leash.</p>
+    if (this.state.trailDesc === null) {
+      // Render loading state...
+      return <div className="fetching"></div>;
+    } else {
+      const trailTags = this.state.trailDesc.tags.map((tag, i) => <span key={i}>{tag}</span>);
+      return (
+        <div className="row flex-row justify-content-left col-8">
+          {/* Template strings (template literals) used for css module format of Bootstrap class names */}
+          {/* See webpack.config.js css-loader options for localIdentName syntax of css output */}
+          <div className="col">
+            <p>{this.state.trailDesc.description}</p>
 
-          <div className={`row ${style.stats}`}>
-            <div className="col">
-              <div className="d-flex justify-content-center">
-                Distance<br />
-                6.1 miles
+            <div className={`row ${style.stats}`}>
+              <div className="col">
+                <div className="d-flex justify-content-center">
+                  Distance<br />
+                  {this.state.trailDesc.distance} {this.state.trailDesc.distanceUnits}
+                </div>
+              </div>
+              <div className="col">
+                <div className="d-flex justify-content-center">
+                  Elevation Gain<br />
+                  {this.state.trailDesc.elevationGain} {this.state.trailDesc.elevationUnits}
+                </div>
+              </div>
+              <div className="col">
+                <div className="d-flex justify-content-center">
+                  Route Type<br />
+                  {this.state.trailDesc.routeType}
+                </div>
               </div>
             </div>
-            <div className="col">
-              <div className="d-flex justify-content-center">
-                Elevation Gain<br />
-                351 feet
-              </div>
-            </div>
-            <div className="col">
-              <div className="d-flex justify-content-center">
-                Route Type<br />
-                Loop
-              </div>
-            </div>
-          </div>
 
-          <div className="row">
-            <div className={`${style.col} ${style.tags}`}>
-              <span>dogs on leash</span> <span>kid friendly</span> <span>birding</span> <span>hiking</span> <span>mountain biking</span>
-              <span>nature trips</span> <span>road biking</span> <span>trail running</span> <span>walking</span> <span>forest</span> <span>lake</span>
-              <span>partially paved</span>
+            <div className="row">
+              <div className={`${style.col} ${style.tags}`}>
+                {trailTags}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
